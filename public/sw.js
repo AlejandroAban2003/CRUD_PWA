ce865fdede097361867a8e6a730e92c5c4c2e7f7
AlejandroAban2003 <abanalejandro90@gmail.com>
2024-11-18 22:35:00 -0600

    Commit

--- public/sw.js
+++ public/sw.js
@@ -1,30 +1,44 @@
+// Caching files for offline access
+const CACHE_NAME = 'v1';
+const CACHE_ASSETS = [
+    'index.html',
+    'styles.css',
+    'app.js',
+    'images/logo.png',
+];
+
+// Install event
 self.addEventListener('install', event => {
     event.waitUntil(
-      caches.open('v1').then(cache => {
-        return cache.addAll([
-          '/',
-          '/styles.css',
-          '/app.js',
-          '/manifest.json'
-        ]);
-      })
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Caching all assets');
+                cache.addAll(CACHE_ASSETS);
+            })
+            .then(() => self.skipWaiting())
     );
-  });
-  
-  self.addEventListener('fetch', event => {
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Service Worker: clearing old cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
     event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
+        fetch(event.request)
+            .catch(() => caches.match(event.request))
     );
-  });
-  
-  let deferredPrompt;
-
-
-  self.addEventListener('sync', event => {
-    if (event.tag === 'sync-events') {
-      event.waitUntil(syncLocalEventsWithServer()); // Sincroniza los eventos locales con el servidor
-    }
-  });
-  
+});
